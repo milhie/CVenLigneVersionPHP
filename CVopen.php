589f55de003ea2c956125b51a105387ab8aa2490
milhie <emiliedelsol85@gmail.com>
2020-01-08 18:40:17 +0100

    Enregistrement de mon CV en ligne au format PHP pour la création d'un formulaire de contact et d'une page avec accès sécurisé par mot de passe pour le téléchargement du CV au format PDF, ça sert a rien mais c'est pour montrer mon savoir faire de débutante

---
+++ CVopen.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" type="text/css" media="all"/>
+        <title>CV Emilie</title>
+    </head>
+
+    <body>
+        <div id="page">
+            
+
+            <aside>
+                <header>
+                    <p id="photoCV"><img src="photoCVs.jpg" alt="ma photoCV" ></p>
+                    <div class="title">
+                        <p><img src="plume.png" alt="plume"></p>
+                        <h3>Contact</h3>
+                    </div>
+                    <p class="content">adresse: <br/>270 rue des mandres 31340 <br/>Mirepoix sur Tarn</p>
+                    <p>Téléphone: <br/>06-47-58-79-21</p>
+                    <p>site web:<a href="http://www.milhiecreation .com"> http://www.milhiecreation.com.</a> </p>
+                    <p>Lien vers mon repository<a href="git@github.com:milhie/CVenLigne.git"> git</a></p>
+                </header>
+
+                <section id="about">
+                    <div class="title">
+                        <p><img src="moi.png" alt="silhouette"></p>
+                        <h3>A propos de moi</h3>
+                    </div>
+                    <p>Passionnée de secourisme depuis mon plus jeune âge, c'est très naturellement que je me suis dirigée vers le métier d'ambulancier. <br/>Ma deuxième passion étant l'informatique et les nouvelles technologies , <br/> c'est tout naturellement que je me dirige vers une reconversion afin d'ouvrir une nouvelle page.</p>
+                </section>
+
+                <section id="Loisirs">
+                    <div class="title">
+                        <p><img src="loisirs.png" alt="loisirs"></p>
+                        <h3>Loisirs:</h3>
+                    </div>
+                    <div class="logos">
+                        <p class="logo"><img src="natation.png" alt="Natation"></p>
+                        <p class="logo"><img src="escalade.png" alt="Escalade"></p>
+                        <p class="logo"><img src="randonnee.png" alt="Randonnée"></p>
+                    </div>
+                    <div class="logos">
+                        <p class="logo"><img src="canicross.png" alt="canicross"></p>
+                        <p class="logo"><a href="http://www/milhiecreation.com" title="milhie creation"><img src="creation.png" alt="Création de bijoux"></a></p>
+                        <p class="logo"><img src="code.png" alt="code"></p>
+                    </div>
+                </section>   
+            </aside>
+
+            <main>
+                <header>
+                    <h1><span id="prenom">Emilie</span> Delsol</h1>
+                    <h2><span id="underline">Etudiante</span> développeuse web </h2>
+                </header>
+
+                <article>
+                    <div class="title">
+                        <p><img src="chapeau.png" alt="chapeau"></p>
+                        <h2>Formation</h2>
+                    
+                    </div>
+                    <table>
+                        <tr>
+                            <th><img src="pucel.png" alt="puce"> 2011:</th>
+                            <td>DEA (Diplôme d'Etat d'Ambulancier)</td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> 2010:</th>
+                            <td>AFGSU II (Attestation de Formaion aux Gestes et Soins d'Urgence niveau 2); dernier recyclage le 03/06/2019</td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> 2009:</th>
+                            <td>PSC1 (Prévention et Secours Civiques de niveau 1)</td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> 2003-2006</th>
+                            <td>IUT Génie mécanique et productique (nieau)</td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> 2003:</th>
+                            <td>BNSSA (Brevet National de Sécurité et de Sauvetage Aquatique)</td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> 2003:</th>
+                            <td>AFCPSAM (Attestation de Formation Complémentaire aux Premiers Secours Avec Matériel)</td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> 2003:</th>
+                            <td>BAC S SI (Sciences de l'Ingénieur)</td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> 2001:</th>
+                            <td>AFPS (Attestaion de Formation aux Premiers Secours)</td>
+                        </tr>
+                    </table>
+                    
+                    <div class="title">
+                        <p><img src="valise.png" alt="valise"></p>
+                        <h2>Expérience professionelle</h2>
+                        
+                    </div>
+
+                    <table>
+                        <tr>
+                            <th><img src="pucel.png" alt="puce"> Depuis 2011<br/>Ambulances St Ambroise <br/>Toulouse</th>
+                            <td>Ambulancière</td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> 2007-2011<br/>Ambulances St Ambroise <br/>Toulouse</th>
+                            <td>Auxiliaire Ambulancière</td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> 2005-2007:<br/>Alacita<br/>Balma</th>
+                            <td>Service en salle et barmaid </td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> Etés 2003-2004-2005:<br/>Plages des Vernhes<br/>Salles-Curan</th>
+                            <td>Surveillance seule d'une plage au bord du lac de Pareloup.</td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> Eté 2005:<br/>Piscine municipale<br/>Rodez</th>
+                            <td>Surveillance en équipe</td>
+                        </tr>
+                    </table>
+                    
+                    <div class="title">
+                        <p><img src="qualite.png" alt="qualités"></p>
+                        <h2>Compétences</h2>
+                        
+                    </div>
+
+                    <ul>
+                        <li>Travail en équipe</li>
+                        <li>Grande capacité d'adaptation</li>
+                        <li>Sang-froid</li>
+                        <li>Empathie </li>
+                        <li>Gestion du stress</li>
+                        <li>Autodidacte</li>
+                    </ul>
+
+                </article>
+            </main>
+            
+        </div>
+       </body>
+     </html>
---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" type="text/css" media="all"/>
+        <title>CV Emilie</title>
+    </head>
+
+    <body>
+        <div id="page">
+            
+
+            <aside>
+                <header>
+                    <p id="photoCV"><img src="photoCV.jpg" alt="ma photoCV" ></p>
+                    <p id="age">34 ans</p>
+                    <div class="title">
+                        <p><img src="plume.png" alt="plume"></p>
+                        <h3>Contact</h3>
+                    </div>
+                    <p class="content">adresse: <br/>270 rue des mandres 31340 <br/>Mirepoix sur Tarn</p>
+                    <p>Téléphone: <br/>06-47-58-79-21</p>
+
+                    <p>site web:<a href="http://www.milhiecreation .com"> http://www.milhiecreation.com.</a> </p>
+                    <p><a href="https://milhie.github.io/CVenLigne/">Mon CV</a> en ligne.</p>
+                    <p>Mon CV en version <a href="http://www.milhiecreation.com/CV/CV-Emilie.pdf"> PDF </a></p>
+                   
+                </header>
+
+                <section id="about">
+                    <div class="title">
+                        <p><img src="moi.png" alt="silhouette"></p>
+                        <h3>A propos de moi</h3>
+                    </div>
+                    <p>Passionnée de secourisme depuis mon plus jeune âge, c'est très naturellement que je me suis dirigée vers le métier d'ambulancier. <br/>Aujourd'hui, souhaitant ouvrir une nouvelle page dans ma vie, je me reconvertit vers ma deuxième passion: l'informatique et les nouvelles technologies.</p>
+                </section>
+
+                <section id="loisirs">
+                    <div class="title">
+                        <p><img src="loisirs.png" alt="loisirs"></p>
+                        <h3>Loisirs:</h3>
+                    </div>
+                    <div class="logos">
+                        <p class="logo"><img src="natation.png" alt="Natation"></p>
+                        <p class="logo"><img src="escalade.png" alt="Escalade"></p>
+                        <p class="logo"><img src="randonnee.png" alt="Randonnée"></p>
+                    </div>
+                    <div class="logos">
+                        <p class="logo"><img src="canicross.png" alt="canicross"></p>
+                        <p class="logo"><a href="http://www/milhiecreation.com" title="milhie creation"><img src="creation.png" alt="Création de bijoux"></a></p>
+                        <p class="logo"><img src="code.png" alt="code"></p>
+                    </div>
+                </section>   
+            </aside>
+
+            <main>
+                <header>
+                    <h1><span id="prenom">Emilie</span> Delsol</h1>
+                    <h2><span id="underline">Etudiante</span> développeuse web </h2>
+                </header>
+
+                <article>
+                    <div class="title">
+                        <p><img src="chapeau.png" alt="chapeau"></p>
+                        <h2>Formation</h2>
+                    
+                    </div>
+                    <table>
+                        <tr>
+                            <th><img src="pucel.png" alt="puce"> 2011:</th>
+                            <td>DEA (Diplôme d'Etat d'Ambulancier)</td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> 2010:</th>
+                            <td>AFGSU II (Attestation de Formaion aux Gestes et Soins d'Urgence niveau 2); dernier recyclage le 03/06/2019</td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> 2009:</th>
+                            <td>PSC1 (Prévention et Secours Civiques de niveau 1)</td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> 2003-2006</th>
+                            <td>IUT Génie mécanique et productique (niveau)</td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> 2003:</th>
+                            <td>BNSSA (Brevet National de Sécurité et de Sauvetage Aquatique)</td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> 2003:</th>
+                            <td>AFCPSAM (Attestation de Formation Complémentaire aux Premiers Secours Avec Matériel)</td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> 2003:</th>
+                            <td>BAC S SI (Sciences de l'Ingénieur)</td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> 2001:</th>
+                            <td>AFPS (Attestaion de Formation aux Premiers Secours)</td>
+                        </tr>
+                    </table>
+                    
+                    <div class="title">
+                        <p><img src="valise.png" alt="valise"></p>
+                        <h2>Expérience professionelle</h2>
+                        
+                    </div>
+
+                    <table>
+                        <tr>
+                            <th><img src="pucel.png" alt="puce"> Depuis 2011<br/>Ambulances St Ambroise <br/>Toulouse</th>
+                            <td>Ambulancière</td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> 2007-2011<br/>Ambulances St Ambroise <br/>Toulouse</th>
+                            <td>Auxiliaire Ambulancière</td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> 2005-2007:<br/>Alacita<br/>Balma</th>
+                            <td>Service en salle et barmaid </td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> Etés 2003-2004-2005:<br/>Plages des Vernhes<br/>Salles-Curan</th>
+                            <td>Surveillance seule d'une plage au bord du lac de Pareloup.</td>
+                        </tr>
+                        <tr>
+                            <th><img src="puce.png" alt="puce"> Eté 2005:<br/>Piscine municipale<br/>Rodez</th>
+                            <td>Surveillance en équipe</td>
+                        </tr>
+                    </table>
+                    
+                    <footer>
+                        <div class="title">
+                            <p><img src="qualite.png" alt="qualités"></p>
+                            <h2>Qualités</h2>
+                        
+                        </div>
+                        <div id="comp">
+                            <ul>
+                                <li>Travail en équipe</li>
+                                <li>Empathie</li>
+                                <li>Ecoute</li>
+                            </ul>
+                            <ul>
+                                <li>Grande capacité d'adaptation</li>
+                                <li>Gestion du stress</li>
+                                <li>Sang-froid</li>
+                            </ul>
+                            <ul>
+                                <li>Riguoureuse</li>
+                                <li>Réactive</li>
+                                <li>Autodidacte</li>
+                            </ul>
+                        </div>
+                    </footer>
+
+                </article>
+            </main>
+            
+        </div>
+       </body>
+     </html>
