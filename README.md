889ff40f23db2dacc294961ba51924b56c01f3b6
milhie <emiliedelsol85@gmail.com>
2020-01-08 19:30:52 +0100

    Création des fichiers contact.php et cible.php pour mon formulaire de contact. Modification du descriptif de la page

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CVenLigneVersionPHP
 Modification de mon CV en ligne HTML en version PHP avec formulaire de contact valide. et page securisée par mot de passe pour le telechargement de mon CV en version PDF
+Ceci afin de montrer mon savoir faire de débutante
---
+++ contact.php
@@ -0,0 +1,25 @@
+
+<p>Mon profil vous plait? Vous pouvez me contacter via le fomulaire ci dessous:</p>
+
+<form method="post" action="cible.php">
+ 
+   	<fieldset>
+       <legend>Vos coordonnées</legend> <!-- Titre du fieldset --> 
+
+       <label for="nom">Quel est votre nom ?</label>
+       <input type="text" name="nom" id="nom" required />
+
+       <label for="email">Votre e-mail:</label>
+       <input type="email" name="email" id="email" required />
+
+       <label for="tph">Numéro de téléphone:</label>
+       <input type="tel" name="tph" id="tph" />
+
+   	</fieldset>
+   	<fieldset>
+   		<label for="message">Votre message:</label>
+   		<textarea name="message" id="message" rows="10" cols="50" required>
+   	</fieldset>
+ 
+   
+</form>
