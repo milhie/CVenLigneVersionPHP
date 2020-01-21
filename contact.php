5501450bae4d6b8fb46bf15fa04dc6dbcc9f73e7
milhie <emiliedelsol85@gmail.com>
2020-01-21 14:53:07 +0100

    mise en page de mon formulaire de contact

--- contact.php
+++ contact.php
@@ -16,33 +16,36 @@
   </head>
   <body>
     <div id="blocpage">
+      <header>
+        <h2>Formulaire de Contact</h2>
+
+        <p>Mon profil vous plait? Vous pouvez me contacter via le formulaire ci-dessous:</p>
+      </header>
+
+      <main>
+    
+        <form id="contact" method="post" action="cible.php">
+          <fieldset>
+            <legend>Vos coordonnées</legend>
+            <p><label for="nom">Nom:</label><input type="text" id="nom" name="nom" value="<?php if (isset($_POST['nom'])){echo$_POST['nom'];} ?> "></p>
+
+            <p><label for="email">Email :</label><input type="text" id="email" name="email" value="<?php if (isset($_POST['mail'])){echo$_POST['mail'];} ?> "/></p>
+
+          </fieldset>
+
+          <fieldset>
+            <legend>Votre message :</legend>
+            <p><label for="objet">Objet :</label><input type="text" id="objet" name="objet" value="<?php if (isset($_POST['objet'])){echo$_POST['objet'];} ?>" >
+            </p>
+
+            <p><label for="message">Message :</label><textarea id="message" name="message" placeholder="votre message" cols="30" rows="8"><?php if (isset($_POST['message'])) {echo $_POST['message'];} ?></textarea></p>
+
+            <p><label for="code">Ecrivez en LETTRES le résultat : <?php echo $nospam['question']; ?></label><input type="text" name="code" id="code" /><input type="hidden" name="nospam_question" value="<?php echo $nospam['num']; ?>" /></p>
+          </fieldset>
       
-
-    <h2>Formulaire de Contact</h2>
-
-    <p>Mon profil vous plait? Vous pouvez me contacter via le formulaire ci-dessous:</p>
-    <form id="contact" method="post" action="cible.php">
-      <fieldset>
-        <legend>Vos coordonnées</legend>
-        <p><label for="nom">Nom:</label><input type="text" id="nom" name="nom" value="<?php if (isset($_POST['nom'])){echo$_POST['nom'];} ?> "></p>
-
-        <p><label for="email">Email :</label><input type="text" id="email" name="email" value="<?php if (isset($_POST['mail'])){echo$_POST['mail'];} ?> "/></p>
-
-      </fieldset>
-
-      <fieldset>
-        <legend>Votre message :</legend>
-        <p><label for="objet">Objet :</label><input type="text" id="objet" name="objet" value="<?php if (isset($_POST['objet'])){echo$_POST['objet'];} ?>" >
-        </p>
-
-        <p><label for="message">Message :</label><textarea id="message" name="message" placeholder="votre message" cols="30" rows="8"><?php if (isset($_POST['message'])) {echo $_POST['message'];} ?></textarea></p>
-
-        <p><label for="code">Ecrivez en LETTRES le résultat : <?php echo $nospam['question']; ?></label><input type="text" name="code" id="code" /><input type="hidden" name="nospam_question" value="<?php echo $nospam['num']; ?>" /></p>
-      </fieldset>
-      
-      <div style="text-align:center;"><input type="submit" name="envoi" value="Envoyer le formulaire !" /></div>
-    </form>
-
-  </div>
+          <div id="bouton"><input type="submit" name="envoi" value="Envoyer le formulaire !" /></div>
+        </form>
+      </main>
+    </div>
   </body>
 </html>
